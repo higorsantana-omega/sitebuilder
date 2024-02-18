5ced17e3deb0804a2210736be5f4d035c3f62cfc
higorsantana-omega <higor.santanahs17@gmail.com>
2024-02-17 23:37:27 -0300

    canvas context

---
+++ src/components/canvas/context/index.tsx
@@ -0,0 +1,68 @@
+import { type Dispatch, type ReactNode, type SetStateAction, createContext, useState } from "react";
+
+export type ElementsType =
+  | "Container"
+
+export type ElementInstance = {
+  id: string;
+  type: ElementsType;
+  extraAttributes?: Record<string, unknown>;
+};
+
+interface CanvasContextValue {
+  elements: ElementInstance[];
+  setElements: Dispatch<SetStateAction<ElementInstance[]>>;
+  addElement: (index: number, element: ElementInstance) => void;
+  removeElement: (id: string) => void
+
+  selectedElement: ElementInstance | null;
+  setSelectedElement: Dispatch<SetStateAction<ElementInstance | null>>
+
+  updateElement: (id: string, element: ElementInstance) => void;
+}
+
+export const CanvasContext = createContext({} as CanvasContextValue)
+
+export function CanvasProvider ({ children }: { children: ReactNode }) {
+  const [elements, setElements] = useState<ElementInstance[]>([]);
+  const [selectedElement, setSelectedElement] = useState<ElementInstance | null>(null)
+
+  const addElement = (index: number, element: ElementInstance) => {
+    setElements((prev) => {
+      const newElements = [...prev];
+      newElements.splice(index, 0, element);
+      return newElements;
+    });
+  };
+
+  const removeElement = (id: string) => {
+    setElements((prev) => prev.filter((element) => element.id !== id));
+  };
+
+  const updateElement = (id: string, element: ElementInstance) => {
+    setElements((prev) => {
+      const newElements = [...prev];
+      const index = newElements.findIndex((el) => el.id === id);
+      newElements[index] = element;
+      return newElements;
+    });
+  };
+
+  return (
+    <CanvasContext.Provider
+      value={{
+        elements,
+        setElements,
+        addElement,
+        removeElement,
+
+        selectedElement,
+        setSelectedElement,
+
+        updateElement,
+      }}
+    >
+      {children}
+    </CanvasContext.Provider>
+  )
+}
