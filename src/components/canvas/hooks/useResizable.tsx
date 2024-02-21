15fdb3df9515da1c1f2ca35c62596a0a2a6325b5
higorsantana-omega <higor.santanahs17@gmail.com>
2024-02-20 22:16:43 -0300

    perf: add usecallback in handle resize

--- src/components/canvas/hooks/useResizable.tsx
+++ src/components/canvas/hooks/useResizable.tsx
@@ -1,16 +1,15 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export function useResizable () {
   const [sizes, setSizes] = useState<number[]>([])
 
-  const handleResize = (index: number, height: number) => {
-    console.log({ index, height })
+  const handleResize = useCallback((index: number, height: number) => {
     const newSizes = [...sizes];
     const currentSize = newSizes[index] ?? 0;
     const newSize = currentSize + height;
     newSizes[index] = newSize;
     setSizes(newSizes);
-  }
+  }, [sizes])
 
   return { sizes, handleResize }
 }
